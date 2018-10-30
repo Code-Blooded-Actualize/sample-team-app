f9ac359b2a8376172a22de65a801345248370f42
“Marksoloff” <marksoloff@gmail.com>
2018-10-29 20:45:56 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/pages' => 'pages#index'
-<<<<<<< HEAD
     get '/snakes' => 'snakes#index'
-=======
     get '/examples' => 'examples#index'
->>>>>>> b378e6075fb0d6ee471b731466fce5e8048976aa
   end
 end
