f5f05e412647d3819bb38cac8d6fbd3a93216cb4
JackMarx <seriousfools@gmail.com>
2018-10-29 19:01:06 -0500

    messages happen

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,6 +1,6 @@
 class Api::PagesController < ApplicationController
   def index
-    @message = "Hello, Ashley was not here. It was Josh, ha ha ha..."
+    @message = "Hello, Ashley was not here. It was Josh"
     render 'index.json.jbuilder'
   end
 end
